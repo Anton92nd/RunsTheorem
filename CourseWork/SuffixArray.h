5ece8f0456273a4c4202b871e4ac48c68072dca2
Anton92nd <anton92nd@gmail.com>
2015-05-31 21:29:29 +0600

    project initiated

---
+++ CourseWork/SuffixArray.h
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+vector<int> calculateSuffixArray(vector<int> s, int sygma)
+{
+	return vector<int>();
+}
---
+++ CourseWork/main.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "SuffixArray.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+vector<int> array, result;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int length, sygma;
+	scanf("%d%d\n", &length, &sygma);
+	array.reserve(length);
+	for (int i = 0; i < length; i++)
+	{
+		char c;
+		scanf("%c", &c);
+		array.push_back(c - 'a' + 1);
+	}
+	result = calculateSuffixArray(array, sygma);
+	for (int i = 0; i < result.size(); i++)
+	{
+		printf("%d ", result[i]);
+	}
+	return 0;
+}

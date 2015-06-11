1bc5c761f2bf8b16ac12aec2b10ddc971144b9fb
Anton92nd <anton92nd@gmail.com>
2015-06-11 17:00:41 +0600

    Tests renamed

---
+++ CourseWork/UnitTests/TestRMQ.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\rmq.h"
+#include <ctime>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(TestRMQ)
+	{
+	private:
+		
+	public:
+
+		
+	};
+}

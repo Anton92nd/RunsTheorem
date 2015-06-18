669abca8186b551e86e31a9adade85e2b8a6149e
Anton92nd <anton92nd@gmail.com>
2015-06-18 19:08:22 +0600

    statistics

--- CourseWork/main.cpp
+++ CourseWork/main.cpp
@@ -5,27 +5,53 @@
 
 using namespace std;
 
-vector<int> array, sa, lcp;
+vector<int> generateRandomString(int size, int sygma)
+{
+	vector<int> result(size + 1);
+	for (int i = 0; i < size; i++)
+	{
+		result[i] = rand() % (sygma - 1) + 1;
+	}
+	return result;
+}
+
+vector<int> array;
+long long runsCount = 0, periodsSum = 0, lengthSum = 0, sumPrecalc = 0, sumRunsTime = 0;
+
+vector<Run> runs;
+
+void addStats()
+{
+	sort(runs.begin(), runs.end());
+	auto it = unique(runs.begin(), runs.end());
+	runs.resize(it - runs.begin());
+	runsCount += runs.size();
+	for (int i = 0; i < runs.size(); i++)
+	{
+		periodsSum += runs[i].p;
+		lengthSum += runs[i].j - runs[i].i + 1;
+	}
+	sumPrecalc += precalcEnd - precalcBegin;
+	sumRunsTime += calcEnd - precalcEnd;
+}
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	int length, sygma;
-	scanf("%d%d\n", &length, &sygma);
-	array.reserve(length);
-	for (int i = 0; i < length; i++)
+	int n, sygma, k;
+	scanf("%d%d%d", &n, &sygma, &k);
+	srand(time(NULL));
+	sygma++;
+	for (int i = 0; i < k; i++)
 	{
-		char c;
-		scanf("%c", &c);
-		array.push_back(c - 'a' + 1);
+		array.clear();
+		array = generateRandomString(n, sygma);
+		runs.clear();
+		runs = calculateRuns(array, sygma);
+		addStats();
 	}
-	array.push_back(0);
-	vector<Run> runs = calculateRuns(array, sygma);
-	printf("Runs:\n");
-	for (int i = 0; i < runs.size(); i++)
-	{
-		printf("%d %d %d\n", runs[i].i + 1, runs[i].j + 1, runs[i].p);
-	}
+	printf("Runs count   | Period   | Length  | Precalc time (ms) | Runs calc time\n%-15.6lf%-11.6lf%-13.6lf%-17.3lf%.3lf",
+		runsCount * 1.0 / k, periodsSum * 1.0 / runsCount, lengthSum * 1.0 / runsCount, sumPrecalc * 1.0 / k, sumRunsTime * 1.0 / k);
 	return 0;
 }
